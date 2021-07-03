da8f5be3432d89314e32e0d979936677f5ce5246
Abhinav-SJ <73390621+Abhinav-SJ@users.noreply.github.com>
2021-07-03 21:17:13 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,4 +1,14 @@
 int swap_max(int a[], int l, int n)
 {
-  
+  int max,i,loc,t;
+  max = a[n];
+  for(i=n;i<l;i++)
+  {
+    if(a[i]>max)
+      max=a[i];
+      loc=i;
+  }
+  t=a[n];
+  a[n]=a[loc];
+  a[loc]=t;
 }
